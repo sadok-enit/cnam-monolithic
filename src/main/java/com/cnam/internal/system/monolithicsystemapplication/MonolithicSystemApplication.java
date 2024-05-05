98170420eadfa840b5213be6356527480450d147
Ghassen KAHRI <ghassen_kahri@ext.carrefour.com>
2024-05-06 00:12:51 +0100

    initialise module

---
+++ src/main/java/com/cnam/internal/system/monolithicsystemapplication/MonolithicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cnam.internal.system.monolithicsystemapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithicSystemApplication.class, args);
+	}
+
+}
