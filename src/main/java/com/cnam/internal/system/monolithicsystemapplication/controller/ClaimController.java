a04e3df57fcd1da66837a61734ca8f76f9a969f0
Ghassen KAHRI <ghassen_kahri@ext.carrefour.com>
2024-05-07 00:57:28 +0100

    add claim feature

--- src/main/java/com/cnam/internal/system/monolithicsystemapplication/controller/ClaimController.java
+++ src/main/java/com/cnam/internal/system/monolithicsystemapplication/controller/ClaimController.java
@@ -1,31 +1,23 @@
 package com.cnam.internal.system.monolithicsystemapplication.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cnam.internal.system.monolithicsystemapplication.model.Claim;
 import com.cnam.internal.system.monolithicsystemapplication.model.ClaimStatus;
-import com.cnam.internal.system.monolithicsystemapplication.repository.ClaimRepository;
 import com.cnam.internal.system.monolithicsystemapplication.service.ClaimService;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class ClaimController {
-
-    @Autowired
-    private ClaimRepository claimRepository;
 
     @Autowired
     private ClaimService claimService;
--- src/main/java/com/cnam/internal/system/monolithicsystemapplication/model/Claim.java
+++ src/main/java/com/cnam/internal/system/monolithicsystemapplication/model/Claim.java
@@ -1,7 +1,11 @@
 package com.cnam.internal.system.monolithicsystemapplication.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,18 +16,41 @@ import lombok.Data;
 @Entity
 @Table(name = "claims")
 public class Claim {
+    //missing attributes will be added later
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "claimer_name")
-    private String title;
+    @Column(name = "numeroBSReclame")
+    private String numeroReclamation;
 
-    @Column(name = "description")
-    private String description;
+    @Column(name = "libelle")
+    private String libelle;
 
-    @Column
+//    @Column(name = "mntEngage")
+//    private BigDecimal mntEngage;
+
+//    @Column(name = "mntReclame")
+//    private BigDecimal mntReclame;
+
+    @Column(name = "motif")
+    private String motif;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @Column(name = "observation")
+    private String observation;
+
+    @Column(name = "bulletin_Code")
+    private long bulletinCode;
+
+//    @Column(name = "numBordereauReclamation")
+//    private String numBordereauReclamation;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private ClaimStatus status = ClaimStatus.PENDING;
 
 }
